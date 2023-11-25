6ce856b40486b5047459d0599e8ac90c6dcea2cb
MMRedFloyd <mirza-m97@hotmail.com>
2023-11-25 15:26:00 +0100

    problem with fetching data

--- src/pages/news.js
+++ src/pages/news.js
@@ -8,12 +8,14 @@ import { globalActions } from "../../store/global-slice";
 
 export default function ItemDetails() {
   const [details, setDetails] = useState([]);
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
-  const loading = useSelector((state) => state.global.loading);
+  // const loading = useSelector((state) => state.global.loading);
   const id = "872585";
 
   useEffect(() => {
     async function fetchItemDetails() {
+      setLoading(true);
       // dispatch(globalActions.setLoading(true));
       const response = await fetch(
         `https://api.themoviedb.org/3/movie/${id}?language=en-US&api_key=${key}`
@@ -44,15 +46,23 @@ export default function ItemDetails() {
 
       setDetails(transformedDetails);
       // dispatch(globalActions.setLoading(false));
+      setLoading(false);
     }
     fetchItemDetails();
-    console.log(details);
+    // console.log(details.genres.map((genre) => genre));
     console.log(details.genres);
     console.log(Array.isArray(details.genres));
+    console.log(details.budget);
     // console.log(loading);
   }, []);
-  // if (!details) {
-  //   // If details is not yet set, you can choose to render a loading state or return null
+
+  console.log(details);
+  if (loading) {
+    // If details is not yet set, you can choose to render a loading state or return null
+    return <p>Loading...</p>;
+  }
+
+  // if (details.length == 0) {
   //   return <p>Loading...</p>;
   // }
 
@@ -73,8 +83,20 @@ export default function ItemDetails() {
         <div className={classes.smallDetails}>
           <div className={classes.genres}>
             {/* {details.genres.map((genre) => (
-                <p className={classes.genre}>{genre}</p>
-              ))} */}
+              <p className={classes.genre} key={genre}>
+                {genre}
+              </p>
+            ))} */}
+            {details.genres ? (
+              details.genres.map((genre) => (
+                <p className={classes.genre} key={genre}>
+                  {genre}
+                </p>
+              ))
+            ) : (
+              <p>Loading genres...</p>
+            )}
+            <div></div>
           </div>
 
           <p>{details.releaseYear}</p>
@@ -105,43 +127,58 @@ export default function ItemDetails() {
 
             <div className={classes.stats}>
               <table>
-                <tr className={classes.listFlex}>
-                  <th className={classes.item}>Released</th>
-                  <td className={classes.value}>
-                    {new Date(details.releaseDate)
-                      .toLocaleDateString("en-UK")
-                      .replace(/\//g, "-")}
-                  </td>
-                </tr>
+                {details && (
+                  <>
+                    <tr className={classes.listFlex}>
+                      <th className={classes.item}>Released</th>
+                      <td className={classes.value}>
+                        {new Date(details.releaseDate)
+                          .toLocaleDateString("en-UK")
+                          .replace(/\//g, "-")}
+                      </td>
+                    </tr>
 
-                <tr className={classes.listFlex}>
-                  <th className={classes.item}>Runtime</th>
-                  <td className={classes.value}>
-                    {details.runtimeHours}h {details.runtimeMinutes}min
-                  </td>
-                </tr>
+                    <tr className={classes.listFlex}>
+                      <th className={classes.item}>Runtime</th>
+                      <td className={classes.value}>
+                        {details.runtimeHours}h {details.runtimeMinutes}min
+                      </td>
+                    </tr>
 
-                <tr className={classes.listFlex}>
-                  <th className={classes.item}>Director</th>
-                  <td className={classes.value}>{details.releaseDate}</td>
-                </tr>
+                    <tr className={classes.listFlex}>
+                      <th className={classes.item}>Director</th>
+                      <td className={classes.value}>{details.releaseDate}</td>
+                    </tr>
 
-                <tr className={classes.listFlex}>
-                  <th className={classes.item}>Budget</th>
-                  <td className={classes.value}>
-                    {/* ${details.budget.toLocaleString("en-US")} */}
-                  </td>
-                </tr>
+                    <tr className={classes.listFlex}>
+                      <th className={classes.item}>Budget</th>
+                      <td className={classes.value}>
+                        {/* ${details.budget.toLocaleString("en-US")} */}
+                        {details.budget
+                          ? `$${Number(details.budget).toLocaleString("en-US")}`
+                          : "Not available"}
+                        {/* {details.budget} */}
+                      </td>
+                    </tr>
 
-                <tr className={classes.listFlex}>
-                  <th className={classes.item}>Genres</th>
-                  {/* <td className={classes.value}>{details.genres.join(", ")}</td> */}
-                </tr>
+                    <tr className={classes.listFlex}>
+                      <th className={classes.item}>Genres</th>
+                      <td className={classes.value}>
+                        {/* {details.genres.join(", ")} */}
+                        {details.genres ? (
+                          details.genres.join(", ")
+                        ) : (
+                          <p>Loading genres...</p>
+                        )}
+                      </td>
+                    </tr>
 
-                <tr className={classes.listFlex}>
-                  <th className={classes.item}>Language</th>
-                  <td className={classes.value}>{details.releaseDate}</td>
-                </tr>
+                    <tr className={classes.listFlex}>
+                      <th className={classes.item}>Language</th>
+                      <td className={classes.value}>{details.releaseDate}</td>
+                    </tr>
+                  </>
+                )}
               </table>
             </div>
           </div>
